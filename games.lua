c416cf95567ac38a44bb6e3a83e8df21aa1b3d77
ChromemasterOnYt <78050954+ChromemasterOnYt@users.noreply.github.com>
2021-02-11 15:35:13 -0500

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,5 +1,8 @@
 {
-  if game.GameId ~= "5293755937" then do
-      print'coming soon!!!"
-  end
+    if game.GameId ~= "5293755937" then do
+      print'coming soon!!!'
+    end
+    else then do
+      print'Game is not compatible.'
+    end
 }
