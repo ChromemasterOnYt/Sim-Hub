2d873bb32b37eab22697a0397a2ab0727a19b9ea
ChromemasterOnYt <78050954+ChromemasterOnYt@users.noreply.github.com>
2021-02-11 15:54:13 -0500

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,9 +1,8 @@
-{
-    if game.GameId ~= "5293755937" then do
-        print('pog')
-        script.Parent.Visible = false
-    end
-    else then do
-      print'Game is not compatible.'
-    end
-}
+
+if game.GameId ~= "5293755937" then do
+    print('pog')
+    script.Parent.Visible = false
+end
+else then
+  print'Game is not compatible.'
+end
