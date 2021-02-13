b6501b6ba76d3f5a317763222dd9cbeff1cbdf2c
ChromemasterOnYt <78050954+ChromemasterOnYt@users.noreply.github.com>
2021-02-12 22:28:58 -0500

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,6 +1,3 @@
-if game.GameId ~= "5293755937" then 
-    print('pog')
-    script.Parent.Visible = false
-else
-    print'Game is not compatible.'
+if game.GameId ~= "5293755937" then
+    print("pog")
 end
